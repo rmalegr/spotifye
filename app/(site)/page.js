ade2a5928b76a9e4496d2a3a4b24a5507660c56a
rmalegr <rodrigomalegre@gmail.com>
2023-06-21 21:55:40 -0300

    add sidebarItem

---
+++ app/(site)/page.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import Todo from '../todos/page'
+import TodoList from '../todos/TodoList'
+
+export default function Home() { 
+  
+  return (
+    <main className="text-green-500">
+      <h1>Hola Spotify again </h1>
+   
+      
+    </main>
+  )
+}
